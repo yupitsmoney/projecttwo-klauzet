9444e502e3b81d532123bb96523ed42f0666535c
Money <money@moneysingh.com>
2015-06-07 16:30:49 -0700

    added controllers & models

---
+++ klauzet/app/controllers/fashionpost_controller.rb
@@ -0,0 +1,2 @@
+class FashionpostController < ApplicationController
+end
---
+++ klauzet/test/models/fashionpost_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FashionpostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
