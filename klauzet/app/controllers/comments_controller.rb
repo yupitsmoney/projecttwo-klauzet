54fa575f3221aeddaad92787782eeb9ccf754b8c
Money <money@moneysingh.com>
2015-06-07 17:02:19 -0700

    added models, controllers, and views

--- klauzet/app/controllers/comments_controller.rb
+++ klauzet/app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+    def create
+        comment = Comment.new(body: params[:comment][:body])
+        post = Post.find(params[:post_id])
+        if comment.valid? && post
+            post.comments.push comment
+            post.save
+            current_user.comments.push comment
+            current_user.save
+            redirect_to post_path(post)
+        else
+            flash["alert-warning"] = "Sorry, comment not created"
+            redirect_to posts_path
+        end
+    end
+
+
+end
+
+	
 end
--- klauzet/app/controllers/targets_controller.rb
+++ klauzet/app/controllers/targets_controller.rb
@@ -1,2 +1,7 @@
 class TargetsController < ApplicationController
+		 before_action :authorize 
+
+  def index
+
+  end
 end
